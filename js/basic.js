8432c3056da40b363f41861122013738f2dd6efb
interactivo830047215 <45860111+interactivo830047215@users.noreply.github.com>
2018-12-13 16:54:31 -0500

    cdo
    cdo

--- js/basic.js
+++ js/basic.js
@@ -34,12 +34,12 @@ var doc = new jsPDF();/*Horizontal
 var string = doc.output ('datauristring');
 
 var iframe = "<iframe width = '100%' height = '100%' src = '" + string + "'> </iframe>"
-
-var x = window.open ();
+$('#loadPDF').html(iframe);
+/*var x = window.open ();
 x.document.open ();
 x.document.write (iframe);
 x.document.close ();
-x.document.reload(true); 
+x.document.reload(true); */
 
 //doc.output ('dataurlnewwindow');
 
